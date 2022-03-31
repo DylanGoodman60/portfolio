2c388487dc235a91d5768bb1c9b3a5bea4111273
Dylan Goodman <dylan.goodman60@gmail.com>
2022-03-30 23:48:57 -0400

    image addition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Sidebar from "./components/sidebar/Sidebar"
+import Mainpage from "./components/mainpage/Mainpage"
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Mainpage />
     </div>
   );
 }
---
+++ src/components/mainpage/Mainpage.jsx
@@ -0,0 +1,10 @@
+import "./mainpage.css"
+import toronto from "./toronto.jpg"
+
+export default function mainpage() {
+    return (
+        <div className='mainpage'>
+            <img src={toronto}/>
+        </div>
+    )
+}
