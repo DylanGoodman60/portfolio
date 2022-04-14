ca1b531ee762f6d13654e7b4f58c6e0170f8b8e7
Dylan Goodman <dylan.goodman60@gmail.com>
2022-04-14 12:39:07 -0400

    social media buttons

--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -3,7 +3,17 @@ import { FaLinkedinIn } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaFilePdf } from 'react-icons/fa';
 
+const resumeUrl = "https://youtube.com";
+const linkedinUrl = "https://www.linkedin.com/in/dylan-jr-goodman/";
+const githubUrl = "https://github.com/dylangoodman60";
+
+
 export default function Navigator() {
+
+    function openLink(str) {
+        window.open(str, '_blank').focus();
+    };
+
     return (
         <div className="navigator">
             <div className ="left">
@@ -24,9 +34,9 @@ export default function Navigator() {
             </div>
             <div className="middle"></div>
             <div className = "right">
-                <FaFilePdf />
-                <FaLinkedinIn />
-                <FaGithub />
+                <FaFilePdf onClick={() => openLink(resumeUrl)} className="fa-icon"/>
+                <FaLinkedinIn onClick={() => openLink(linkedinUrl)}/>
+                <FaGithub onClick={() => openLink(githubUrl)}/>
             </div>
         </div>
     )
