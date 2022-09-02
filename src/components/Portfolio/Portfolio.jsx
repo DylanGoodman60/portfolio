be27d1831bbd6363a4385482f44bcf292fa8c8ac
Dylan Goodman <dylan.goodman60@gmail.com>
2022-09-02 13:58:38 -0400

    fix alt tags

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,16 +1,9 @@
-import {useRef} from 'react';
-
 import "./Portfolio.css"
 import Maffle from "./Maffle/Maffle"
 import Sonova from "./Sonova/Sonova"
 import Guelph from './Guelph/Guelph';
 
 export default function Portfolio({screen}) {
-    const ref = useRef(null);
-
-    const handleClick = () => {
-        ref.current?.scrollIntoView({behavior: 'smooth'});
-    };
 
     if (screen !== 'portfolio') {
         return null;
