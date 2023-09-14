32071751a08d72b3fd28c611ee23554a338b401d
Dylan Goodman <dylan.goodman60@gmail.com>
2023-09-14 13:45:25 -0400

    SAP portfolio portion

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,6 +2,8 @@ import "./Portfolio.css"
 import Maffle from "./Maffle/Maffle"
 import Sonova from "./Sonova/Sonova"
 import Guelph from './Guelph/Guelph';
+import SAP from './SAP/SAP';
+
 
 export default function Portfolio({screen}) {
 
@@ -10,6 +12,7 @@ export default function Portfolio({screen}) {
     }
     return (
         <div className="portfolio">
+            <SAP />
             <Guelph />
             <Sonova />
             <Maffle />
